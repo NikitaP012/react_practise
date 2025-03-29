49e49023ce6b077f1825bb940ba251e2612d21b1
Nikita Phapale <phapalenikita80@gmail.com>
2025-03-30 00:11:45 +0530

    initial_commit

---
+++ react/src/Neha.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Neha = () => {
+  return (
+    <div>
+    Name : <h1>Nikita Dipak Phapale</h1>
+    Age : <h1>23</h1>
+    company:<h2>Prisms communication</h2>
+    Address:<h3>Jachwadi</h3>
+    </div>
+  )
+}
+
+export default Neha
